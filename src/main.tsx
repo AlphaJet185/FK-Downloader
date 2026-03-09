b9f2fe3b533e7e5db3e1c13094471d7940a52331
AlphaJet185 <falih.m@outlook.in>
2026-03-09 03:33:34 +0300

    tailwind error

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";       // <-- add .tsx extension
-import "./index.css";              // <-- add .css extension
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css'; // <- this is important
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
